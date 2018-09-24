78d5006573cdbaef3f86062a7121fdcf696c31f8
Karthik <karthikitinnovation@gmail.com>
2018-09-24 21:25:53 +0530

    this is first commit in local repo

---
+++ H.java
@@ -0,0 +1,11 @@
+public class H
+	{
+
+	public static void main(String[] args) 
+	
+	{
+		// TODO Auto-generated method stub
+		System.out.println("programm to print 1 - 10");
+	}
+
+}
---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		for(int=1;i<=10;i++)
+		{
+			system.out.println("Hello Word");
+		}
+	}
+}
